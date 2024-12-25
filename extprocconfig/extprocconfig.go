57371296c0ad0f1fb318b1718adfb482506bf667
Takeshi Yoneda <t.y.mathetake@gmail.com>
2024-12-25 14:29:29 +0900

    more
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- extprocconfig/extprocconfig.go
+++ extprocconfig/extprocconfig.go
@@ -8,6 +8,14 @@ package extprocconfig
 
 import gwapiv1 "sigs.k8s.io/gateway-api/apis/v1"
 
+// Config is the configuration for the external processor.
+type Config struct {
+	InputSchema             VersionedAPISchema `json:"inputSchema"`
+	ModelNameHeaderKey      string             `json:"modelNameHeaderKey"`
+	BackendRoutingHeaderKey string             `json:"backendRoutingHeaderKey"`
+	Rules                   []RouteRule        `json:"rules"`
+}
+
 // VersionedAPISchema corresponds to LLMAPISchema in api/v1alpha1/api.go.
 type VersionedAPISchema struct {
 	Schema  APISchema
@@ -22,14 +30,6 @@ const (
 	APISchemaAWSBedrock APISchema = "AWSBedrock"
 )
 
-// Config is the configuration for the external processor.
-type Config struct {
-	InputSchema             VersionedAPISchema
-	ModelNameHeaderKey      string
-	BackendRoutingHeaderKey string
-	Rules                   []RouteRule
-}
-
 // HeaderMatch is an alias for HTTPHeaderMatch of the Gateway API.
 type HeaderMatch = gwapiv1.HTTPHeaderMatch
 
@@ -37,18 +37,18 @@ type HeaderMatch = gwapiv1.HTTPHeaderMatch
 // besides the `Backends` field is modified to abstract the concept of a backend
 // at Envoy Gateway level to a simple name.
 type RouteRule struct {
-	Headers  []HeaderMatch
-	Backends []Backend
+	Headers  []HeaderMatch `json:"headers"`
+	Backends []Backend     `json:"backends"`
 }
 
 // Backend corresponds to LLMRouteRuleBackendRef in api/v1alpha1/api.go
-// besides this abstracts the concept of a backend at Envoy Gateway level to a simple name.
+// besides that this abstracts the concept of a backend at Envoy Gateway level to a simple name.
 type Backend struct {
 	// Name of the backend, which is the value in the final routing decision
 	// matching the header key specified in the [Config.BackendRoutingHeaderKey].
-	Name string
+	Name string `json:"name"`
 	// OutputSchema specifies the API schema of the output format of requests from.
-	OutputSchema VersionedAPISchema
+	OutputSchema VersionedAPISchema `json:"outputSchema"`
 	// Weight is the weight of the backend in the routing decision.
-	Weight int
+	Weight int `json:"weight"`
 }
