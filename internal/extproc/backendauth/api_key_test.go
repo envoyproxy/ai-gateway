2ee446c18ca89c4bcc0448db812b5be7a151a0d4
Aaron Choo <achoo30@bloomberg.net>
2025-01-17 13:23:49 -0500

    fix api key tests
    Signed-off-by: Aaron Choo <achoo30@bloomberg.net>

--- internal/extproc/backendauth/api_key_test.go
+++ internal/extproc/backendauth/api_key_test.go
@@ -1,3 +1,63 @@
 package backendauth
 
-// TODO-AC: Add tests
+import (
+	"github.com/envoyproxy/ai-gateway/filterconfig"
+	corev3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+	extprocv3 "github.com/envoyproxy/go-control-plane/envoy/service/ext_proc/v3"
+	"github.com/stretchr/testify/require"
+	"os"
+	"testing"
+)
+
+func TestNewAPIKeyHandler(t *testing.T) {
+	auth := filterconfig.APIKeyAuth{Filename: "test"}
+	handler, err := NewAPIKeyHandler(&auth)
+	require.NoError(t, err)
+	require.NotNil(t, handler)
+}
+
+func TestApiKeyHandler_Do(t *testing.T) {
+	f, err := os.Create("/tmp/test")
+	require.NoError(t, err)
+
+	_, err = f.WriteString("test")
+	require.NoError(t, err)
+	err = f.Sync()
+	require.NoError(t, err)
+	err = f.Close()
+	require.NoError(t, err)
+
+	auth := filterconfig.APIKeyAuth{Filename: "/tmp/test"}
+	handler, err := NewAPIKeyHandler(&auth)
+	require.NoError(t, err)
+	require.NotNil(t, handler)
+
+	secret, err := os.ReadFile(auth.Filename)
+	require.NoError(t, err)
+	require.Equal(t, "test", string(secret))
+
+	requestHeaders := map[string]string{":method": "POST"}
+	headerMut := &extprocv3.HeaderMutation{
+		SetHeaders: []*corev3.HeaderValueOption{
+			{Header: &corev3.HeaderValue{
+				Key:   ":path",
+				Value: "/model/some-random-model/converse",
+			}},
+		},
+	}
+	bodyMut := &extprocv3.BodyMutation{
+		Mutation: &extprocv3.BodyMutation_Body{
+			Body: []byte(`{"messages": [{"role": "user", "content": [{"text": "Say this is a test!"}]}]}`),
+		},
+	}
+	err = handler.Do(requestHeaders, headerMut, bodyMut)
+	require.NoError(t, err)
+
+	bearerToken, ok := requestHeaders["Authorization"]
+	require.True(t, ok)
+	require.Equal(t, "Bearer test", bearerToken)
+
+	require.Equal(t, 2, len(headerMut.SetHeaders))
+	require.Equal(t, "Authorization", headerMut.SetHeaders[1].Header.Key)
+	require.Equal(t, []byte("Bearer test"), headerMut.SetHeaders[1].Header.GetRawValue())
+}
