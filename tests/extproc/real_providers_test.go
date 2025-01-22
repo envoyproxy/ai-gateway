d39060f01f85e1ea7d690d40bc121ed23337a462
Aaron Choo <achoo30@bloomberg.net>
2025-01-22 10:33:11 -0500

    remove env testing temporarily
    Signed-off-by: Aaron Choo <achoo30@bloomberg.net>

--- tests/extproc/real_providers_test.go
+++ tests/extproc/real_providers_test.go
@@ -76,23 +76,17 @@ func TestWithRealProviders(t *testing.T) {
 			},
 			{
 				Backends: []filterconfig.Backend{
-					{Name: "aws-bedrock-credentials-file", Schema: awsBedrockSchema, Auth: &filterconfig.BackendAuth{AWSAuth: &filterconfig.AWSAuth{
+					{Name: "aws-bedrock", Schema: awsBedrockSchema, Auth: &filterconfig.BackendAuth{AWSAuth: &filterconfig.AWSAuth{
 						CredentialFileName: awsFilePath,
 						Region:             "us-east-1",
 					}}},
 				},
 				Headers: []filterconfig.HeaderMatch{{Name: "x-model-name", Value: "us.meta.llama3-2-1b-instruct-v1:0"}},
 			},
-			{
-				Backends: []filterconfig.Backend{
-					{Name: "aws-bedrock-env", Schema: awsBedrockSchema, Auth: &filterconfig.BackendAuth{AWSAuth: &filterconfig.AWSAuth{}}},
-				},
-				Headers: []filterconfig.HeaderMatch{{Name: "x-model-name", Value: "us.meta.llama3-2-1b-instruct-v1:0"}},
-			},
 		},
 	})
 
-	requireExtProcWithAWSCredentials(t, configPath)
+	//requireExtProcWithAWSCredentials(t, configPath)
 
 	t.Run("health-checking", func(t *testing.T) {
 		client := openai.NewClient(option.WithBaseURL(listenerAddress + "/v1/"))
@@ -100,9 +94,8 @@ func TestWithRealProviders(t *testing.T) {
 			testCaseName,
 			modelName string
 		}{
-			{testCaseName: "openai", modelName: "gpt-4o-mini"},                                             // This will go to "openai"
-			{testCaseName: "aws-bedrock-credentials-file", modelName: "us.meta.llama3-2-1b-instruct-v1:0"}, // This will go to "aws-bedrock" using credentials file.
-			{testCaseName: "aws-bedrock-env", modelName: "us.meta.llama3-2-1b-instruct-v1:0"},              // This will go to "aws-bedrock" using env.
+			{testCaseName: "openai", modelName: "gpt-4o-mini"},                            // This will go to "openai"
+			{testCaseName: "aws-bedrock", modelName: "us.meta.llama3-2-1b-instruct-v1:0"}, // This will go to "aws-bedrock" using credentials file.
 		} {
 			t.Run(tc.modelName, func(t *testing.T) {
 				require.Eventually(t, func() bool {
@@ -170,9 +163,8 @@ func TestWithRealProviders(t *testing.T) {
 			testCaseName,
 			modelName string
 		}{
-			{testCaseName: "openai", modelName: "gpt-4o-mini"},                                             // This will go to "openai"
-			{testCaseName: "aws-bedrock-credentials-file", modelName: "us.meta.llama3-2-1b-instruct-v1:0"}, // This will go to "aws-bedrock" using credentials file.
-			{testCaseName: "aws-bedrock-env", modelName: "us.meta.llama3-2-1b-instruct-v1:0"},              // This will go to "aws-bedrock" using env.
+			{testCaseName: "openai", modelName: "gpt-4o-mini"},                            // This will go to "openai"
+			{testCaseName: "aws-bedrock", modelName: "us.meta.llama3-2-1b-instruct-v1:0"}, // This will go to "aws-bedrock" using credentials file.
 		} {
 			t.Run(tc.modelName, func(t *testing.T) {
 				require.Eventually(t, func() bool {
