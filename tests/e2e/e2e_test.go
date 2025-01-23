371bc3ed9d77b494d37faf80ba5a23ffd715714c
Takeshi Yoneda <t.y.mathetake@gmail.com>
2025-01-23 13:55:01 -0800

    examples: adds token rate limit
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- tests/e2e/e2e_test.go
+++ tests/e2e/e2e_test.go
@@ -60,6 +60,11 @@ func TestMain(m *testing.M) {
 		panic(err)
 	}
 
+	if err := initRateLimitServer(ctx); err != nil {
+		cancel()
+		panic(err)
+	}
+
 	code := m.Run()
 	cancel()
 	os.Exit(code)
@@ -178,6 +183,24 @@ func initTestupstream(ctx context.Context) (err error) {
 	}
 	initLog("\twaiting for deployment")
 	return kubectlWaitForDeploymentReady("default", "testupstream")
+}
+
+func initRateLimitServer(ctx context.Context) (err error) {
+	initLog("Installing Redis for Rate limits")
+	start := time.Now()
+	defer func() {
+		elapsed := time.Since(start)
+		initLog(fmt.Sprintf("\tdone (took %.2fs in total)\n", elapsed.Seconds()))
+	}()
+	initLog("\tapplying manifests")
+	if err = kubectlApplyManifest(ctx, "./init/ratelimit/"); err != nil {
+		return
+	}
+	initLog("\twaiting for deployment")
+	if err := kubectlWaitForDeploymentReady("redis-system", "redis"); err != nil {
+		return err
+	}
+	return kubectlWaitForDeploymentReady("envoy-gateway-system", "envoy-ratelimit")
 }
 
 func kubectl(ctx context.Context, args ...string) *exec.Cmd {
