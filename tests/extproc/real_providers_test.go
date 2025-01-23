e305c1573d61f54fb8b292ab01a288f18453f1cb
Alexa Griffith <agriffith96@gmail.com>
2025-01-23 17:08:05 -0500

    fix tools again lol
    Signed-off-by: Alexa Griffith <agriffith96@gmail.com>

--- tests/extproc/real_providers_test.go
+++ tests/extproc/real_providers_test.go
@@ -221,23 +221,24 @@ func TestWithRealProviders(t *testing.T) {
 						Messages: openai.F([]openai.ChatCompletionMessageParamUnion{
 							openai.UserMessage("What is the weather like in Paris today?"),
 						}),
-						Functions: []openai.FunctionDefinition{
+						Tools: openai.F([]openai.ChatCompletionToolParam{
 							{
-								Name:        "get_weather",
-								Description: "Get current temperature for a given location.",
-								Parameters: map[string]interface{}{
-									"type": "object",
-									"properties": map[string]interface{}{
-										"location": map[string]interface{}{
-											"type":        "string",
-											"description": "City and country e.g. Paris, France",
+								Type: openai.F(openai.ChatCompletionToolTypeFunction),
+								Function: openai.F(openai.FunctionDefinitionParam{
+									Name:        openai.String("get_weather"),
+									Description: openai.String("Get weather at the given location"),
+									Parameters: openai.F(openai.FunctionParameters{
+										"type": "object",
+										"properties": map[string]interface{}{
+											"location": map[string]string{
+												"type": "string",
+											},
 										},
-									},
-									"required":             []interface{}{"location"},
-									"additionalProperties": false,
-								},
+										"required": []string{"location"},
+									}),
+								}),
 							},
-						},
+						}),
 						Model: openai.F(tc.modelName),
 					})
 					if err != nil {
