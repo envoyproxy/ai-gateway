5f3b64ad383d9e3291b9ce68fa672510740e8e1d
Alexa Griffith <agriffith96@gmail.com>
2025-01-24 15:40:39 -0500

    update rule
    Signed-off-by: Alexa Griffith <agriffith96@gmail.com>

--- tests/extproc/real_providers_test.go
+++ tests/extproc/real_providers_test.go
@@ -81,7 +81,10 @@ func TestWithRealProviders(t *testing.T) {
 						Region:             "us-east-1",
 					}}},
 				},
-				Headers: []filterconfig.HeaderMatch{{Name: "x-model-name", Value: "us.meta.llama3-2-1b-instruct-v1:0"}},
+				Headers: []filterconfig.HeaderMatch{
+					{Name: "x-model-name", Value: "us.meta.llama3-2-1b-instruct-v1:0"},
+					{Name: "us.anthropic.claude-3-5-sonnet-20240620-v1:0"}
+				},
 			},
 		},
 	})
@@ -212,7 +215,7 @@ func TestWithRealProviders(t *testing.T) {
 			testCaseName,
 			modelName string
 		}{
-			{testCaseName: "aws-bedrock-anthropic", modelName: "bedrock.us.anthropic.claude-3-5-sonnet-20240620-v1:0"}, // This will go to "aws-bedrock" using credentials file.
+			{testCaseName: "aws-bedrock", modelName: "us.anthropic.claude-3-5-sonnet-20240620-v1:0"}, // This will go to "aws-bedrock" using credentials file.
 		} {
 			t.Run(tc.modelName, func(t *testing.T) {
 				require.Eventually(t, func() bool {
