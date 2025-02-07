71f537a594f74523e2a5eaa7b4d5b7b2bfb37c10
Aaron Choo <achoo30@bloomberg.net>
2025-02-07 11:49:52 -0500

    backend security policy tests
    Signed-off-by: Aaron Choo <achoo30@bloomberg.net>

--- internal/controller/backend_security_policy_test.go
+++ internal/controller/backend_security_policy_test.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -24,6 +25,17 @@ func TestBackendSecurityController_Reconcile(t *testing.T) {
 
 	err := cl.Create(context.Background(), &aigv1a1.BackendSecurityPolicy{ObjectMeta: metav1.ObjectMeta{Name: backendSecurityPolicyName, Namespace: namespace}})
 	require.NoError(t, err)
+	err = cl.Create(context.Background(), &aigv1a1.BackendSecurityPolicy{
+		ObjectMeta: metav1.ObjectMeta{Name: fmt.Sprintf("%s-OIDC", backendSecurityPolicyName), Namespace: namespace},
+		Spec: aigv1a1.BackendSecurityPolicySpec{
+			Type: aigv1a1.BackendSecurityPolicyTypeAWSCredentials,
+			AWSCredentials: &aigv1a1.BackendSecurityPolicyAWSCredentials{
+				Region:            "us-east-1",
+				OIDCExchangeToken: &aigv1a1.AWSOIDCExchangeToken{},
+			},
+		},
+	})
+	require.NoError(t, err)
 	_, err = c.Reconcile(context.Background(), reconcile.Request{NamespacedName: types.NamespacedName{Namespace: namespace, Name: backendSecurityPolicyName}})
 	require.NoError(t, err)
 	item, ok := <-ch
@@ -31,6 +43,15 @@ func TestBackendSecurityController_Reconcile(t *testing.T) {
 	require.IsType(t, &aigv1a1.BackendSecurityPolicy{}, item)
 	require.Equal(t, backendSecurityPolicyName, item.(*aigv1a1.BackendSecurityPolicy).Name)
 	require.Equal(t, namespace, item.(*aigv1a1.BackendSecurityPolicy).Namespace)
+
+	// Test backendSecurityPolicy with OIDC credentials have the annotation added
+	oidcBackendSecurityPolicy := &aigv1a1.BackendSecurityPolicy{}
+	err = cl.Get(context.Background(), types.NamespacedName{Namespace: namespace, Name: fmt.Sprintf("%s-OIDC", backendSecurityPolicyName)}, oidcBackendSecurityPolicy)
+	require.NoError(t, err)
+	require.Len(t, oidcBackendSecurityPolicy.Annotations, 1)
+	time, ok := oidcBackendSecurityPolicy.Annotations["refresh"]
+	require.True(t, ok)
+	require.NotEmpty(t, time)
 
 	// Test the case where the BackendSecurityPolicy is being deleted.
 	err = cl.Delete(context.Background(), &aigv1a1.BackendSecurityPolicy{ObjectMeta: metav1.ObjectMeta{Name: backendSecurityPolicyName, Namespace: namespace}})
