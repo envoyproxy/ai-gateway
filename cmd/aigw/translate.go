1f152ef88f5c1c807a1eda653b5815098bfb6b33
Takeshi Yoneda <t.y.mathetake@gmail.com>
2025-03-01 02:09:02 +0900

    more
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- cmd/aigw/translate.go
+++ cmd/aigw/translate.go
@@ -34,6 +34,7 @@ import (
 	"github.com/envoyproxy/ai-gateway/internal/controller"
 )
 
+// translateFn is the function signature for the translate command for decoupling and testing.
 type translateFn func(cmd cmdTranslate, stdout, stderr io.Writer) error
 
 // translate implements the translateFn command. This function reads the input files, collects the AI Gateway custom resources,
