5e16765463a4fe29fc8a8713c03ee25024bb1385
Takeshi Yoneda <t.y.mathetake@gmail.com>
2025-03-05 12:52:02 -0800

    fix
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- cmd/aigw/run_test.go
+++ cmd/aigw/run_test.go
@@ -1,9 +1,15 @@
+// Copyright Envoy AI Gateway Authors
+// SPDX-License-Identifier: Apache-2.0
+// The full text of the Apache license is available in the LICENSE file at
+// the root of the repo.
+
 package main
 
 import (
-	"github.com/stretchr/testify/require"
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestRun_default(t *testing.T) {
