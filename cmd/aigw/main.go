ad4e007be6e3aabda99b63b1d8288697148ec28b
Takeshi Yoneda <t.y.mathetake@gmail.com>
2025-03-20 10:29:15 -0700

    more
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- cmd/aigw/main.go
+++ cmd/aigw/main.go
@@ -35,8 +35,9 @@ type (
 	}
 	// cmdRun corresponds to `aigw run` command.
 	cmdRun struct {
-		Debug bool   `help:"Enable debug logging emitted to stderr."`
-		Path  string `arg:"" name:"path" optional:"" help:"Path to the AI Gateway configuration yaml file. Optional. When this is not given, aigw runs the default configuration." type:"path"`
+		Debug       bool   `help:"Enable debug logging emitted to stderr."`
+		Path        string `arg:"" name:"path" optional:"" help:"Path to the AI Gateway configuration yaml file. Optional. When this is not given, aigw runs the default configuration. Use --show-default to check the default configuration's behavior'" type:"path"`
+		ShowDefault bool   `help:"Show the default configuration, and exit."`
 	}
 )
 
