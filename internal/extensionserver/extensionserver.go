aeb24c8bb3f8236a16782ab12728094fe63125d3
Takeshi Yoneda <t.y.mathetake@gmail.com>
2025-03-24 12:45:11 -0700

    try adding extension server
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- internal/extensionserver/extensionserver.go
+++ internal/extensionserver/extensionserver.go
@@ -9,10 +9,14 @@ import (
 	"context"
 
 	pb "github.com/envoyproxy/gateway/proto/extension"
+	clusterv3 "github.com/envoyproxy/go-control-plane/envoy/config/cluster/v3"
+	routev3 "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
+	matcherv3 "github.com/envoyproxy/go-control-plane/envoy/type/matcher/v3"
 	"github.com/go-logr/logr"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/health/grpc_health_v1"
 	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/durationpb"
 )
 
 // Server is the implementation of the EnvoyGatewayExtensionServer interface.
@@ -35,4 +39,65 @@ func (s *Server) Check(context.Context, *grpc_health_v1.HealthCheckRequest) (*gr
 // Watch implements [grpc_health_v1.HealthServer].
 func (s *Server) Watch(*grpc_health_v1.HealthCheckRequest, grpc_health_v1.Health_WatchServer) error {
 	return status.Error(codes.Unimplemented, "Watch is not implemented")
+}
+
+const (
+	originalDstClusterName = "original_destination_cluster"
+	originalDstHeaderName  = "x-ai-eg-original-dst"
+)
+
+// PostTranslateModify allows an extension to modify the clusters and secrets in the xDS config.
+//
+// Currently, this adds an ORIGINAL_DST cluster to the list of clusters unconditionally.
+func (s *Server) PostTranslateModify(_ context.Context, req *pb.PostTranslateModifyRequest) (*pb.PostTranslateModifyResponse, error) {
+	clusters := make([]*clusterv3.Cluster, len(req.Clusters), len(req.Clusters)+1)
+	copy(clusters, req.Clusters)
+	clusters = append(clusters, &clusterv3.Cluster{
+		Name:                 originalDstClusterName,
+		ClusterDiscoveryType: &clusterv3.Cluster_Type{Type: clusterv3.Cluster_ORIGINAL_DST},
+		LbPolicy:             clusterv3.Cluster_CLUSTER_PROVIDED,
+		LbConfig: &clusterv3.Cluster_OriginalDstLbConfig_{
+			OriginalDstLbConfig: &clusterv3.Cluster_OriginalDstLbConfig{
+				UseHttpHeader: true, HttpHeaderName: originalDstHeaderName,
+			},
+		},
+		ConnectTimeout:  &durationpb.Duration{Seconds: 60},
+		DnsLookupFamily: clusterv3.Cluster_V4_ONLY,
+	})
+	response := &pb.PostTranslateModifyResponse{
+		Clusters: clusters,
+		Secrets:  req.Secrets,
+	}
+	return response, nil
+}
+
+// PostVirtualHostModify allows an extension to modify the virtual hosts in the xDS config.
+//
+// Currently, this adds a route that matches on the x-use-original-dst header to the virtual host.
+func (s *Server) PostVirtualHostModify(_ context.Context, req *pb.PostVirtualHostModifyRequest) (*pb.PostVirtualHostModifyResponse, error) {
+	if req.VirtualHost == nil {
+		return nil, nil
+	}
+	for _, route := range req.VirtualHost.Routes {
+		if route.Name == originalDstClusterName {
+			// The route already exists, no need to add it again
+			return nil, nil
+		}
+	}
+	req.VirtualHost.Routes = append(req.VirtualHost.Routes, &routev3.Route{
+		Name: originalDstClusterName,
+		Match: &routev3.RouteMatch{
+			Headers: []*routev3.HeaderMatcher{
+				{
+					Name: "x-ai-eg-use-original-dst",
+					HeaderMatchSpecifier: &routev3.HeaderMatcher_StringMatch{
+						StringMatch: &matcherv3.StringMatcher{
+							MatchPattern: &matcherv3.StringMatcher_Exact{Exact: "true"},
+						},
+					},
+				},
+			},
+		},
+	})
+	return &pb.PostVirtualHostModifyResponse{VirtualHost: req.VirtualHost}, nil
 }
