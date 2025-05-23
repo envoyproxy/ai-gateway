153f07577fd1aa0d09bd32851b68b94810de65b7
Takeshi Yoneda <t.y.mathetake@gmail.com>
2025-05-23 12:36:48 -0700

    CA bundle patch
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -6,17 +6,23 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net"
 	"os"
+	"path/filepath"
 
 	egextension "github.com/envoyproxy/gateway/proto/extension"
 	"go.uber.org/zap/zapcore"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/health/grpc_health_v1"
+	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	clientcfg "sigs.k8s.io/controller-runtime/pkg/client/config"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 
@@ -34,6 +40,7 @@ type flags struct {
 	tlsCertDir                  string
 	tlsCertName                 string
 	tlsKeyName                  string
+	caBundleName                string
 	envoyGatewaySystemNamespace string
 }
 
@@ -77,6 +84,11 @@ func parseAndValidateFlags(args []string) (flags, error) {
 		"/certs",
 		"The directory containing the TLS certificate and key for the webhook server.",
 	)
+	caBundleName := fs.String(
+		"caBundleName",
+		"ca.crt",
+		"The name of the CA bundle file.",
+	)
 	tlsCertName := fs.String(
 		"tlsCertName",
 		"tls.crt",
@@ -90,7 +102,7 @@ func parseAndValidateFlags(args []string) (flags, error) {
 	envoyGatewaySystemNamespace := fs.String(
 		"envoyGatewaySystemNamespace",
 		"envoy-gateway-system",
-		"The namespace where the Envoy Gateway system components are installed. This is used to set the default namespace for the controller manager.",
+		"The namespace where the Envoy Gateway system components are installed.",
 	)
 
 	if err := fs.Parse(args); err != nil {
@@ -119,6 +131,7 @@ func parseAndValidateFlags(args []string) (flags, error) {
 		tlsCertDir:                  *tlsCertDir,
 		tlsCertName:                 *tlsCertName,
 		tlsKeyName:                  *tlsKeyName,
+		caBundleName:                *caBundleName,
 		envoyGatewaySystemNamespace: *envoyGatewaySystemNamespace,
 	}, nil
 }
@@ -162,6 +175,16 @@ func main() {
 		os.Exit(1)
 	}
 
+	cli, err := client.New(clientcfg.GetConfigOrDie(), client.Options{Scheme: controller.Scheme})
+	if err != nil {
+		setupLog.Error(err, "failed to create client")
+		os.Exit(1)
+	}
+	if err := maybePatchAdmissionWebhook(ctx, cli, filepath.Join(flags.tlsCertDir, flags.caBundleName)); err != nil {
+		setupLog.Error(err, "failed to patch admission webhook")
+		os.Exit(1)
+	}
+
 	// Start the extension server running alongside the controller.
 	const extProcUDSPath = "/etc/ai-gateway-extproc-uds/run.sock"
 	s := grpc.NewServer()
@@ -189,4 +212,35 @@ func main() {
 	}); err != nil {
 		setupLog.Error(err, "failed to start controller")
 	}
+}
+
+// This should be the name of the mutating webhook configuration in helm chart.
+const mutatingWebhookConfigurationName = "envoy-ai-gateway-gateway-pod-mutator"
+
+// maybePatchAdmissionWebhook checks if the CA bundle in the mutating webhook configuration installed as part of the
+// helm chart is the same as the one specified in the bundlePath. If not, it updates the CA bundle in the webhook configuration.
+//
+// This allows users to only need to manage the secret and not take care of mutating webhook configuration.
+func maybePatchAdmissionWebhook(ctx context.Context, cli client.Client, bundlePath string) error {
+	webhookConfigName := fmt.Sprintf("%s.%s", mutatingWebhookConfigurationName, os.Getenv("POD_NAMESPACE"))
+	webhookCfg := &admissionregistrationv1.MutatingWebhookConfiguration{}
+	if err := cli.Get(ctx, client.ObjectKey{Name: webhookConfigName}, webhookCfg); err != nil {
+		return fmt.Errorf("failed to get mutating webhook configuration: %w", err)
+	}
+
+	if len(webhookCfg.Webhooks) != 1 {
+		return fmt.Errorf("expected 1 webhook in %s, got %d", webhookConfigName, len(webhookCfg.Webhooks))
+	}
+
+	bundle, err := os.ReadFile(bundlePath)
+	if err != nil {
+		return fmt.Errorf("failed to read CA bundle: %w", err)
+	}
+	if !bytes.Equal(webhookCfg.Webhooks[0].ClientConfig.CABundle, bundle) {
+		webhookCfg.Webhooks[0].ClientConfig.CABundle = bundle
+		if err := cli.Update(ctx, webhookCfg); err != nil {
+			return fmt.Errorf("failed to update mutating webhook configuration: %w", err)
+		}
+	}
+	return nil
 }
