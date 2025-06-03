16d53acd479fa0ee67a1d7c7f8dbf3c74d49fbee
Curtis Maddalozzo <cmaddalozzo@bloomberg.net>
2025-06-03 10:00:41 -0400

    Add priority to backendRef. Set priority on cluster endpoints in extension server.
    Signed-off-by: Curtis Maddalozzo <cmaddalozzo@bloomberg.net>

--- internal/extensionserver/extensionserver.go
+++ internal/extensionserver/extensionserver.go
@@ -187,6 +187,7 @@ func (s *Server) maybeModifyCluster(cluster *clusterv3.Cluster) {
 		backendRef := httpRouteRule.BackendRefs[i]
 		name := backendRef.Name
 		namespace := aigwRoute.Namespace
+		endpoints.Priority = *backendRef.Priority
 		// We populate the same metadata for all endpoints in the LoadAssignment.
 		// This is because currently, an extproc cannot retrieve the endpoint set level metadata.
 		for _, endpoint := range endpoints.LbEndpoints {
