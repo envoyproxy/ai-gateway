711c2103f6ec1297082cd7311355643e75a095e7
Takeshi Yoneda <t.y.mathetake@gmail.com>
2025-07-22 09:21:48 -0700

    Revert "chore: removes codespell"
    This reverts commit 1dcc08413bab6ef9f6b85f17533bb9efe34ec449.

--- Makefile.tools.mk
+++ Makefile.tools.mk
@@ -7,11 +7,14 @@ LOCALBIN ?= $(shell pwd)/.bin
 $(LOCALBIN):
 	mkdir -p $(LOCALBIN)
 
+CODESPELL = $(LOCALBIN)/.venv/codespell@v2.3.0/bin/codespell
 YAMLLINT = $(LOCALBIN)/.venv/yamllint@1.35.1/bin/yamllint
 
 .bin/.venv/%:
 	mkdir -p $(@D)
 	python3 -m venv $@
 	$@/bin/pip3 install $$(echo $* | sed 's/@/==/')
+
+$(CODESPELL): .bin/.venv/codespell@v2.3.0
 
 $(YAMLLINT): .bin/.venv/yamllint@1.35.1
