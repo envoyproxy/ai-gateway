74fbfc5b26aecb5e176eee0880a0ea7bedf07599
Takeshi Yoneda <t.y.mathetake@gmail.com>
2025-08-04 13:19:23 -0700

    controller: skipping user-applied HTTPRoute is not an error
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- internal/extensionserver/post_translate_modify.go
+++ internal/extensionserver/post_translate_modify.go
@@ -164,7 +164,7 @@ func (s *Server) maybeModifyCluster(cluster *clusterv3.Cluster) {
 	if err != nil {
 		if apierrors.IsNotFound(err) {
 			s.log.Info("Skipping user-created HTTPRoute cluster modification",
-				"cluster_name", cluster.Name, "namespace", httpRouteNamespace, "name")
+				"namespace", httpRouteNamespace, "name", httpRouteName)
 			return
 		}
 		s.log.Error(err, "failed to get AIGatewayRoute object",
