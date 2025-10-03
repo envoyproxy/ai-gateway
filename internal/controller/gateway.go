1e8db6e50e96b1a15c81fa13071e7ec2f644404d
Takeshi Yoneda <t.y.mathetake@gmail.com>
2025-10-03 16:37:38 -0700

    refactor: drops unneeded field from filter api
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- internal/controller/gateway.go
+++ internal/controller/gateway.go
@@ -181,7 +181,6 @@ func (c *GatewayController) reconcileFilterConfigSecret(
 ) error {
 	// Precondition: aiGatewayRoutes is not empty as we early return if it is empty.
 	ec := &filterapi.Config{UUID: uuid}
-	ec.ModelNameHeaderKey = internalapi.ModelNameHeaderKeyDefault
 	var err error
 	llmCosts := map[string]struct{}{}
 	for i := range aiGatewayRoutes {
@@ -275,8 +274,6 @@ func (c *GatewayController) reconcileFilterConfigSecret(
 			}
 		}
 	}
-
-	ec.MetadataNamespace = aigv1a1.AIGatewayFilterMetadataNamespace
 
 	// Configuration for MCP processor.
 	ec.MCPConfig = mcpConfig(mcpRoutes)
