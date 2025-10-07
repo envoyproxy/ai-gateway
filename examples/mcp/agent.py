1612e9c3953a7249f753978c853b42470314df86
Ignasi Barrera <ignasi@tetrate.io>
2025-10-07 17:23:33 +0200

    rename
    Signed-off-by: Ignasi Barrera <ignasi@tetrate.io>

--- examples/mcp/agent.py
+++ examples/mcp/agent.py
@@ -3,7 +3,7 @@
 # The full text of the Apache license is available in the LICENSE file at
 # the root of the repo.
 
-# run like this: uv run --exact -q --env-file .env main.py
+# run like this: uv run --exact -q --env-file .env agent.py
 #
 # Customizing the ".env" like:
 #
