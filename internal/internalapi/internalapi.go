04e149603a295d74792431d72df45b548d4cb141
Takeshi Yoneda <t.y.mathetake@gmail.com>
2025-10-17 10:01:59 -0700

    fix
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- internal/internalapi/internalapi.go
+++ internal/internalapi/internalapi.go
@@ -22,7 +22,7 @@ const (
 	// InternalMetadataBackendNameKey is the key used to store the backend name
 	InternalMetadataBackendNameKey = "per_route_rule_backend_name"
 	// MCPBackendHeader is the special header key used to specify the target backend name.
-	MCPBackendHeader = EnvoyAIGatewayHeaderPrefix + "eg-mcp-backend"
+	MCPBackendHeader = EnvoyAIGatewayHeaderPrefix + "mcp-backend"
 	// MCPRouteHeader is the special header key used to identify the mcp route.
 	MCPRouteHeader = EnvoyAIGatewayHeaderPrefix + "mcp-route"
 	// MCPBackendListenerPort is the port for the MCP backend listener.
