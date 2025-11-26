8da6b4af87cb7bd1489eac6e0340e97b7554e58b
Takeshi Yoneda <t.y.mathetake@gmail.com>
2025-11-26 12:23:35 -0800

    feat: init dynamic modules work
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

---
+++ internal/dynamicmodule/sdk/gosdk.go
@@ -0,0 +1,121 @@
+// Copyright Envoy AI Gateway Authors
+// SPDX-License-Identifier: Apache-2.0
+// The full text of the Apache license is available in the LICENSE file at
+// the root of the repo.
+
+package sdk
+
+import "io"
+
+// NewHTTPFilterConfig is a function that creates a new HTTPFilter that corresponds to each filter configuration in the Envoy filter chain.
+// This is a global variable that should be set in the init function in the program once.
+//
+// The function is called once globally. The function is only called by the main thread,
+// so it does not need to be thread-safe.
+var NewHTTPFilterConfig func(name string, config []byte) HTTPFilterConfig
+
+// HTTPFilterConfig is an interface that represents a single http filter in the Envoy filter chain.
+// It is used to create HTTPFilter(s) that correspond to each Http request.
+//
+// This is only created once per module configuration via the NewHTTPFilter function.
+type HTTPFilterConfig interface {
+	// NewFilter is called for each new Http request.
+	// Note that this must be concurrency-safe as it can be called concurrently for multiple requests.
+	NewFilter() HTTPFilter
+}
+
+// EnvoyHTTPFilter is an interface that represents the underlying Envoy filter.
+// This is passed to each event hook of the HTTPFilter.
+//
+// **WARNING**: This must not outlive each event hook since there's no guarantee that the EnvoyHTTPFilter will be valid after the event hook is returned.
+// To perform the asynchronous operations, use [EnvoyHTTPFilter.NewScheduler] to create a [Scheduler] and perform the operations in a separate Goroutine.
+// Then, use the [Scheduler.Commit] method to commit the event to the Envoy filter on the correct worker thread to continue processing the request.
+type EnvoyHTTPFilter interface {
+	// GetRequestHeader gets the first value of the request header. Returns the value and true if the header is found.
+	GetRequestHeader(key string) (string, bool)
+	// GetRequestHeaders gets all the request headers.
+	GetRequestHeaders() map[string][]string
+	// SetRequestHeader sets the request header. Returns true if the header is set successfully.
+	SetRequestHeader(key string, value []byte) bool
+	// GetResponseHeader gets the first value of the response header. Returns the value and true if the header is found.
+	GetResponseHeader(key string) (string, bool)
+	// GetResponseHeaders gets all the response headers.
+	GetResponseHeaders() map[string][]string
+	// SetResponseHeader sets the response header. Returns true if the header is set successfully.
+	SetResponseHeader(key string, value []byte) bool
+	// GetRequestBody gets the request body. Returns the io.Reader and true if the body is found.
+	GetRequestBody() (BodyReader, bool)
+	// DrainRequestBody drains n bytes from the request body. This will invalidate the io.Reader returned by GetRequestBody before this is called.
+	DrainRequestBody(n int) bool
+	// AppendRequestBody appends the data to the request body. This will invalidate the io.Reader returned by GetRequestBody before this is called.
+	AppendRequestBody(data []byte) bool
+	// GetResponseBody gets the response body. Returns the io.Reader and true if the body is found.
+	GetResponseBody() (BodyReader, bool)
+	// DrainResponseBody drains n bytes from the response body. This will invalidate the io.Reader returned by GetResponseBody before this is called.
+	DrainResponseBody(n int) bool
+	// AppendResponseBody appends the data to the response body. This will invalidate the io.Reader returned by GetResponseBody before this is called.
+	AppendResponseBody(data []byte) bool
+	// SendLocalReply sends a local reply to the client. This must not be used in after returning continue from the response headers phase.
+	SendLocalReply(statusCode uint32, headers [][2]string, body []byte)
+	// GetDynamicMetadataString gets the dynamic metadata value for the given namespace and key. Returns the value and true if the value is found.
+	GetDynamicMetadataString(namespace string, key string) (string, bool)
+	// GetUpstreamHostMetadataString gets the upstream host metadata value for the given namespace and key. Returns the value and true if the value is found.
+	GetUpstreamHostMetadataString(namespace string, key string) (string, bool)
+	// SetDynamicMetadataString sets the dynamic metadata value for the given namespace and key. Returns true if the value is set successfully.
+	SetDynamicMetadataString(namespace string, key string, value string) bool
+}
+
+type BodyReader interface {
+	// Len returns the length of the body in bytes, regardless of how much has been read.
+	Len() int
+	io.Reader
+}
+
+// HTTPFilter is an interface that represents each Http request.
+//
+// This is created for each new Http request and is destroyed when the request is completed.
+type HTTPFilter interface {
+	// RequestHeaders is called when the request headers are received.
+	RequestHeaders(e EnvoyHTTPFilter, endOfStream bool) RequestHeadersStatus
+	// RequestBody is called when the request body is received.
+	RequestBody(e EnvoyHTTPFilter, endOfStream bool) RequestBodyStatus
+	// ResponseHeaders is called when the response headers are received.
+	ResponseHeaders(e EnvoyHTTPFilter, endOfStream bool) ResponseHeadersStatus
+	// ResponseBody is called when the response body is received.
+	ResponseBody(e EnvoyHTTPFilter, endOfStream bool) ResponseBodyStatus
+}
+
+// RequestHeadersStatus is the return value of the HTTPFilter.RequestHeaders.
+type RequestHeadersStatus int
+
+const (
+	// RequestHeadersStatusContinue is returned when the operation should continue.
+	RequestHeadersStatusContinue                  RequestHeadersStatus = 0
+	RequestHeadersStatusStopIteration             RequestHeadersStatus = 1
+	RequestHeadersStatusStopAllIterationAndBuffer RequestHeadersStatus = 3
+)
+
+// RequestBodyStatus is the return value of the HTTPFilter.RequestBody event.
+type RequestBodyStatus int
+
+const (
+	RequestBodyStatusContinue               RequestBodyStatus = 0
+	RequestBodyStatusStopIterationAndBuffer RequestBodyStatus = 1
+)
+
+// ResponseHeadersStatus is the return value of the HTTPFilter.ResponseHeaders event.
+type ResponseHeadersStatus int
+
+const (
+	ResponseHeadersStatusContinue                  ResponseHeadersStatus = 0
+	ResponseHeadersStatusStopIteration             ResponseHeadersStatus = 1
+	ResponseHeadersStatusStopAllIterationAndBuffer ResponseHeadersStatus = 3
+)
+
+// ResponseBodyStatus is the return value of the HTTPFilter.ResponseBody event.
+type ResponseBodyStatus int
+
+const (
+	ResponseBodyStatusContinue               ResponseBodyStatus = 0
+	ResponseBodyStatusStopIterationAndBuffer ResponseBodyStatus = 1
+)
