912edfe4b03e01d006e16d0039a7bfeb69c90a1c
Takeshi Yoneda <t.y.mathetake@gmail.com>
2025-12-26 11:46:37 -0800

    more
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- cmd/extproc/mainlib/main_test.go
+++ cmd/extproc/mainlib/main_test.go
@@ -213,6 +213,7 @@ func TestExtProcStartupMessage(t *testing.T) {
 	tmpDir := t.TempDir()
 	configPath := filepath.Join(tmpDir, "config.yaml")
 	require.NoError(t, os.WriteFile(configPath, []byte(`
+version: dev
 backends:
 - name: openai
   schema:
