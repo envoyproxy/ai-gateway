00dd702035d5bce1599cee49e9841c9fc4304b2f
Yan Avlasov <yavlasov@google.com>
2026-01-13 03:45:14 +0000

    post merge
    Signed-off-by: Yan Avlasov <yavlasov@google.com>

--- api/v1alpha1/client/informers/externalversions/api/v1alpha1/quotapolicy.go
+++ api/v1alpha1/client/informers/externalversions/api/v1alpha1/quotapolicy.go
@@ -46,7 +46,7 @@ func NewQuotaPolicyInformer(client versioned.Interface, namespace string, resync
 // one. This reduces memory footprint and number of connections to the server.
 func NewFilteredQuotaPolicyInformer(client versioned.Interface, namespace string, resyncPeriod time.Duration, indexers cache.Indexers, tweakListOptions internalinterfaces.TweakListOptionsFunc) cache.SharedIndexInformer {
 	return cache.NewSharedIndexInformer(
-		&cache.ListWatch{
+		cache.ToListWatcherWithWatchListSemantics(&cache.ListWatch{
 			ListFunc: func(options v1.ListOptions) (runtime.Object, error) {
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
@@ -71,7 +71,7 @@ func NewFilteredQuotaPolicyInformer(client versioned.Interface, namespace string
 				}
 				return client.AigatewayV1alpha1().QuotaPolicies(namespace).Watch(ctx, options)
 			},
-		},
+		}, client),
 		&aigatewayapiv1alpha1.QuotaPolicy{},
 		resyncPeriod,
 		indexers,
